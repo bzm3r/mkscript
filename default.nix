e1b0b690a24d766118872a51934282ae9bee2f76
Brian Merchant <bzm3r@proton.me>
2023-12-12 14:25:10 -0800

    init

---
+++ default.nix
@@ -0,0 +1,19 @@
+let pkgs = import <nixpkgs> {};
+in pkgs.callPackage ({ lib, rustPlatform }:
+
+  rustPlatform.buildRustPackage rec {
+    pname = "create-script";
+    version = "unstable-2023-12-12";
+
+    src = ./.;
+
+    cargoLock = { lockFile = ./Cargo.lock; };
+
+    meta = with lib; {
+      description = "Helper for creating a new Rust scripting project";
+      homepage = "https://github.com/bzm3r/create_script";
+      license = with licenses; [ asl20 mit ];
+      maintainers = with maintainers; [ ];
+      mainProgram = "create-script";
+    };
+  }) {}
